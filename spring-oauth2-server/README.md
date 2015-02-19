2afdd907558743cedb3ef9655f883daf30a52909
Mohan Ambalavanan <mohan.ambalavanan@gmail.com>
2015-02-19 23:46:10 +1100

    Spring Authorization server complete

--- spring-oauth2-server/README.md
+++ spring-oauth2-server/README.md
@@ -1,1 +1,38 @@
-Spring OAuth2 Configuration Server Implementation
+Spring MVC Based OAuth2 Authorization Server Example
+===========================================================
+
+Build
+-----
+<pre>
+<code>
+mvn clean install
+</code>
+</pre>
+Run WebApp
+----------
+<pre>
+<code>
+mvn jetty:run
+</code>
+</pre>
+
+OAuth2 Token Test
+-----------------
+<pre>
+<code>
+curl -u test:secret "http://localhost:8080/oauth-server/oauth/token?grant_type=password&username=user&password=password" -X POST
+</code>
+</pre>
+Response
+--------
+<pre>
+<code>
+{
+  "access_token": "9a71eb75-a3df-4ab4-ad1b-94d7af3cb8a9",
+  "token_type": "bearer",
+  "expires_in": 43188,
+  "scope": "read write"
+}
+
+</code>
+</pre>
--- spring-oauth2-server/src/main/java/org/test/config/WebConfig.java
+++ spring-oauth2-server/src/main/java/org/test/config/WebConfig.java
@@ -12,40 +12,63 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@EnableAuthorizationServer
+@EnableWebMvc
 @EnableWebSecurity
-public class WebConfig extends AuthorizationServerConfigurerAdapter {
-
-    @Bean
+public class WebConfig extends WebSecurityConfigurerAdapter {
     @Autowired
-    public AuthenticationManager createAuthenticationManager( AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-        authenticationManagerBuilder
-                .inMemoryAuthentication().withUser("test")
-                .password("test").roles("USER").and()
-                .withUser("admin").roles("USER", "ADMIN");
-        return authenticationManagerBuilder.build();
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
     }
 
-    @Override
-    public void configure(final AuthorizationServerSecurityConfigurer security) throws Exception {
-        super.configure(security);
+    /**
+     * Spring does expose default authentication manager for some
+     * reason we have to expose ourself
+     * @return
+     * @throws Exception
+     */
+    @Bean(name = "authenticationManager")
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
 
-    @Override
-    public void configure(final ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory().withClient("test")
-                .authorizedGrantTypes("password");
-    }
+    @Configuration
+    @EnableAuthorizationServer
+    public static class OAuthConfig extends AuthorizationServerConfigurerAdapter {
 
-    @Override
-    public void configure(final AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        //endpoints.authenticationManager(createAuthenticationManager());
+
+
+        private AuthenticationManager authenticationManager;
+        @Autowired
+        public  void init(AuthenticationManager authenticationManager){
+
+             this.authenticationManager =authenticationManager;
+        }
+
+        @Override
+        public void configure(final AuthorizationServerSecurityConfigurer security) throws Exception {
+            super.configure(security);
+        }
+
+        @Override
+        public void configure(final ClientDetailsServiceConfigurer clients) throws Exception {
+            clients.inMemory().withClient("test").secret("secret")
+                    .authorizedGrantTypes("password").scopes("read","write");
+        }
+
+        @Override
+        public void configure(final AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            endpoints.authenticationManager(authenticationManager);
+        }
     }
 }
