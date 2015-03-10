2d296b5c0f82acf3a795f9f89c56d7f31a4a373c
mohan82 <megeek69>
2015-03-10 16:04:14 +1100

    Added unit test inteceptor implementation in progress

--- spring-angular/app/js/authprovider.js
+++ spring-angular/app/js/authprovider.js
@@ -37,6 +37,15 @@
         this.setAuthInfo = function (authInfo) {
             _authInfo = authInfo;
         };
+
+        this.hasAccessToken = function () {
+            return _authInfo.accessToken;
+        };
+
+        this.hasRefreshToken = function () {
+            return _authInfo.refreshToken;
+        };
+
         this.toString = function () {
             return "accessToken:" + _authInfo.accessToken +
                 "isLoggedIn :" + _authInfo.isLoggedIn +
@@ -46,6 +55,49 @@
         };
         return this;
     });
+
+    authProvider.factory("bearerTokenInterceptor", ["authInfoService", function (authInfoService) {
+        this.request = function (config) {
+
+            if (authInfoService.hasAccessToken()) {
+                config.headers['Authorization'] = 'Bearer ' + authInfoService.getAuthInfo().accessToken;
+            }
+
+            return config;
+
+        };
+
+        return this;
+
+    }]);
+
+    authProvider.factory("refreshTokenInterceptor", ["authInfoService", "$q", function (authInfoService, $q) {
+
+        var self = this;
+
+        var HTTP_UNAUTHORIZED = 401,
+            OAUTH_UNAUTHORIZED_CODE = "unauthorized";
+
+        this.isTokenRefreshable = function (status, data) {
+             return (authInfoService.hasRefreshToken() &&
+                     status === HTTP_UNAUTHORIZED && data.error===OAUTH_UNAUTHORIZED_CODE);
+        };
+
+        this.responseError = function (rejection) {
+
+            if (self.isTokenRefreshable(rejection.status, rejection.data)) {
+                console.log("Token Refreshable Status :%s Data :%s", rejection.status, rejection.data.error);
+
+            } else {
+                console.log(" Token not refreshable Status ", rejection.status, rejection.data.error);
+            }
+            return $q.reject(rejection);
+
+        };
+
+        return this;
+
+    }]);
 
     authProvider.run(["$rootScope", "authInfoService", "OAUTH_CONSTANT", "$log", function ($rootScope, authInfoService,
                                                                                            OAUTH_CONSTANT, $log) {
@@ -58,4 +110,11 @@
         });
 
     }]);
+
+    authProvider.config(["$httpProvider", function ($httpProvider) {
+        $httpProvider.interceptors.push("bearerTokenInterceptor");
+        $httpProvider.interceptors.push("refreshTokenInterceptor");
+
+    }])
+
 })();
